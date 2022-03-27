dbd022c12dd2e5e14794b811ec76d1b0408df793
oscarballard <60292160+oscarballard@users.noreply.github.com>
2022-03-27 14:52:24 -0600

    add new client

--- app/src/App.js
+++ app/src/App.js
@@ -1,30 +1,24 @@
-import React from 'react';
-import { Header } from './components/Header';
 import logo from './logo.svg';
-import './css/App.css';
+import './App.css';
 
 function App() {
   return (
-
-    <React.Fragment>
-      <Header/>
-    </React.Fragment>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
